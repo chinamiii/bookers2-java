4176a0c2b3f3cf34ff9571b8f087e5f5681ca892
chinamiii <chinami.2929@icloud.com>
2021-09-02 13:47:34 +0000

    [9/2]

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,8 +11,7 @@ class Book < ApplicationRecord
 
     validates :title, presence: true
 
-    validates :body, presence: true,
-                     length: { maximum: 200 }
+    validates :body, presence: true, length: { maximum: 200 }
 
 
 
